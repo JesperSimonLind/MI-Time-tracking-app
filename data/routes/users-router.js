cdbbb2ce4d9a63152d74e77ceaaec176d9a2fddd
Malin Nilsson <malinnilsson87@gmail.com>
2022-02-27 17:02:50 +0100

    Login and signup page styling for mobile + tablet

--- data/routes/users-router.js
+++ data/routes/users-router.js
@@ -12,5 +12,9 @@ router.get('/signup', (req, res) => {
     res.render('users/users-create')
 })
 
+router.get('/dashboard', (req, res) => {
+    res.render('users/users-dashboard')
+})
+
 
 module.exports = router;
