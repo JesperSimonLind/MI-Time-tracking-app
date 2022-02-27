8917d345225ec01244c8a759e145c795b4a5752a
Malin Nilsson <malinnilsson87@gmail.com>
2022-02-27 16:43:10 +0100

    Added styling  for sign up page

--- data/routes/users-router.js
+++ data/routes/users-router.js
@@ -8,5 +8,9 @@ router.get('/', (req, res) => {
     res.send("Hello Users router")
 })
 
+router.get('/signup', (req, res) => {
+    res.render('users/users-create')
+})
+
 
 module.exports = router;
