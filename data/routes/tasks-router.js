32e858d3323063b1f45bdcf1606342c4c4a77b71
PeppZZon <jespersimonlind@gmail.com>
2022-02-28 13:34:20 +0100

    Setup files, and added background

--- data/routes/tasks-router.js
+++ data/routes/tasks-router.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
 
 // ROUTES //
 
-router.get('/', (req, res) => {
-    res.send("Hello Tasks router")
-})
+router.get("/", (req, res) => {
+  res.send("Hello Tasks router");
+});
 
+router.get("/create-task", (req, res) => {
+  res.render("tasks/tasks-create");
+});
 
 module.exports = router;
