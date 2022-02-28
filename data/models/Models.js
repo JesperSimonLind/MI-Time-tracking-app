d6803afef6ccdab6cf10b0493db90b5248cbbcaa
Alex <alexandrhall@gmail.com>
2022-02-28 16:48:04 +0100

    models

---
+++ data/models/Models.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    admin: { type: Boolean, required: true },
+    profilePicture: { type: String, required: false },
+    tasks: { type: taskSchema, required: false },
+});
+
+const taskSchema = new mongoose.Schema({
+    category: { type: Array, required: true },
+    description: { type: String, required: true },
+    hours: { type: Number, required: true },
+    public: { type: Boolean, required: true },
+    crated: { type: Date, required: true },
+});
+
+const UsersModel = mongoose.model("Users", userSchema);
+const TasksModel = mongoose.model("Tasks", taskSchema);
+
+module.exports = { UsersModel, TasksModel };
