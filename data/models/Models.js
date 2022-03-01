4f5cfca310e1b02d9bd6f3fd2bc07ad958191095
Alexander <alexandrhall@gmail.com>
2022-03-01 12:28:58 +0100

    stavfel

--- data/models/Models.js
+++ data/models/Models.js
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true },
-    admin: { type: Boolean, required: true },
-    profilePicture: { type: String, required: false },
-    tasks: { type: taskSchema, required: false },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  admin: { type: Boolean, required: true },
+  profilePicture: { type: String, required: false },
+  tasks: { type: taskSchema, required: false },
 });
 
 const taskSchema = new mongoose.Schema({
-    category: { type: Array, required: true },
-    description: { type: String, required: true },
-    hours: { type: Number, required: true },
-    public: { type: Boolean, required: true },
-    crated: { type: Date, required: true },
+  category: { type: Array, required: true },
+  description: { type: String, required: true },
+  hours: { type: Number, required: true },
+  public: { type: Boolean, required: true },
+  created: { type: Date, required: true },
 });
 
 const UsersModel = mongoose.model("Users", userSchema);
