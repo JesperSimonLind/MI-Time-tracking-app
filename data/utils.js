70362d3c2e1e3ced5624ce64832fe6f92dc227b2
Alex <alexandrhall@gmail.com>
2022-03-02 15:20:45 +0100

    token

--- data/utils.js
+++ data/utils.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const hashPassword = (password) => {
     const hashValue = bcrypt.hashSync(password, 8);
@@ -8,6 +10,17 @@ const hashPassword = (password) => {
 const comparePassword = (password, hash) => {
     const correct = bcrypt.compareSync(password, hash);
     return correct;
+};
+
+const forceAuthorize = (req, res, next) => {
+    const { token } = req.cookies;
+
+    if (token && jwt.verify(token, process.env.JWTSECRET)) {
+        const tokenData = jwt.decode(token, process.env.JWTSECRET);
+        next();
+    } else {
+        res.sendStatus(401);
+    }
 };
 
 module.exports = { hashPassword, comparePassword };
