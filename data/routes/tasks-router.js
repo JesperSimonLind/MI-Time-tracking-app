1826e5afa43698c827fe6df8cefd8b3a9d42f485
PeppZZon <jespersimonlind@gmail.com>
2022-03-02 15:23:04 +0100

    Added some function to creating tasks

--- data/routes/tasks-router.js
+++ data/routes/tasks-router.js
@@ -5,26 +5,42 @@ const { UsersModel, TasksModel } = require("../models/Models.js");
 // ROUTES //
 
 router.get("/", (req, res) => {
-    res.send("Hello Tasks router");
+  res.send("Hello Tasks router");
 });
 
-router.get("/list", (req, res) => {
-    res.render("tasks/tasks-list");
+router.get("/list", async (req, res) => {
+  const taskCollection = await TasksModel.find().lean();
+  res.render("tasks/tasks-list", { taskCollection });
 });
 
 router.get("/single", (req, res) => {
-    res.render("tasks/tasks-single");
+  res.render("tasks/tasks-single");
 });
 
 router.get("/update", (req, res) => {
-    res.render("tasks/tasks-update");
+  res.render("tasks/tasks-update");
 });
 
 router.get("/delete", (req, res) => {
-    res.render("tasks/tasks-delete");
+  res.render("tasks/tasks-delete");
 });
 router.get("/create", (req, res) => {
-    res.render("tasks/tasks-create");
+  res.render("tasks/tasks-create");
+});
+
+router.post("/create", async (req, res) => {
+  const { category, description, hours, public, created } = req.body;
+  const newTask = new TasksModel({
+    category: category,
+    description: description,
+    hours: hours,
+    public: true,
+    created: Date.now(),
+  });
+
+  const collection = await newTask.save();
+  console.log(collection);
+  res.redirect("/tasks/list");
 });
 
 module.exports = router;
